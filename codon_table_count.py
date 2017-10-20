c71f6ef5af68cba9f8c36b847ab42dae165c5e6f
Jason Stajich <jason.stajich@ucr.edu>
2017-10-20 13:08:05 -0700

    more description of the report format

--- codon_table_count.py
+++ codon_table_count.py
@@ -2,7 +2,17 @@
 
 codon_file = 'codon_table_compact.txt'
 
+codon_count = 0
+aa_count    = 0
+aa_fourfold = 0
 with open(codon_file,"r") as fh:
     for line in fh:
         line = line.strip() #  remove trailing \n
-        print(line)
+        AA = "" # you should write code to get this from the string line
+        codon_ct = 0 # you should write code to get this from the string line
+        print("Amino acid {} is coded for by {} codons".format(AA,codon_ct))
+
+print("===")
+print("There are {} total amino acids and {} codons".format(aa_count,codon_count))
+print("There are {} AAs which are four-fold or six-fold degenerate".format(aa_fourfold))
+
