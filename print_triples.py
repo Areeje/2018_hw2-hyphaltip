e50eed095b0398e2126ed4c5e295a22bf9404a0a
Jason Stajich <jason.stajich@gmail.com>
2018-10-24 16:16:35 -0700

    problem 1

--- print_triples.py
+++ print_triples.py
@@ -5,4 +5,13 @@ end  = 100
 
 print("Preparing to print numbers {}..{}".format(start,end))
 
+for num in range(start,end+1,1):
+    star = ""
+    if num > 0 and (num % 3) == 0:
+        star = "*"
+#    if num == 0:
+#        star = ""
+        
+    print(num,star)
+    
 # Your code goes next
